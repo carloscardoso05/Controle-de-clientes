a720c0c3f51fe5f633488a3508fca4bfd6660c7f
carloos-05 <cvcardos5@gmail.com>
2023-01-12 23:27:37 -0300

    Ordenando débitos

--- src/components/debts/CostumersDebts.vue
+++ src/components/debts/CostumersDebts.vue
@@ -1,24 +1,24 @@
 <template>
     <div>
         <span class="flex place-content-between">
-            <h2 class="text-2xl font-medium text-left ml-4">{{ costumer.name }}</h2>
+            <h2 class="text-2xl font-medium text-left ml-4">{{ costumerData.name }}</h2>
         </span>
         <div class="text-left ml-8 border-l-2 pl-1 border-gray-400 flex place-content-between"
-            v-for="debt in costumer.debts" :key="debt.dateTime.toString()">
+            v-for="debt in costumerDebts" :key="debt.id">
             <div>
                 <p>{{ capitalize(debt.description) }}: {{ formatMoney(debt.price) }}</p>
-                <p>{{ getDateString(debt.id) }}</p>
+                <p>{{ getDateString((debt.dateTime.toJSON() as any)["seconds"] * 1000) }}</p>
             </div>
             <div>
-                <EditDebtModal :debt="debt" :costumerName="costumer.name"/>
-                <DeleteDebtModal :debt="debt" :costumerName="costumer.name" />
+                <EditDebtModal :debt="debt" :costumerName="costumerData.name"/>
+                <DeleteDebtModal :debt="debt" :costumerName="costumerData.name" />
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { defineProps, computed } from "vue"
+import { defineProps, computed, PropType } from "vue"
 import DeleteDebtModal from "./DeleteDebtModal.vue";
 import EditDebtModal from "./EditDebtModal.vue";
 import formatMoney from "@/util/formatMoney";
@@ -26,11 +26,15 @@ import getDateString from "@/util/getDateString";
 import capitalize from "@/util/capitalize";
 import ICostumer from "@/interfaces/ICostumer";
 
-const costumer = computed(() => props.costumerData as ICostumer)
+const costumerDebts = computed(() => {
+    const debts = props.costumerData.debts;
+    debts.sort((a, b) => a.id - b.id)
+    return debts
+})
 
 const props = defineProps({
     costumerData: {
-        type: Object,
+        type: Object as PropType<ICostumer>,
         required: true
     }
 })
