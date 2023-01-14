8916cbd6bbbaac4d6d66c06a7319d8b82f2a2d48
carloos-05 <cvcardos5@gmail.com>
2023-01-14 15:07:27 -0300

    Update CostumersDebts.vue

--- src/components/debts/CostumersDebts.vue
+++ src/components/debts/CostumersDebts.vue
@@ -1,18 +1,18 @@
 <template>
 	<div class="rounded-md max-w-3xl mx-auto space-y-6 border shadow-inner py-4">
 		<div class="xs:mx-8 mx-2 mt-2 border-b border-neutral-400 flex justify-between">
-			<h1 class="text-2xl font-medium text-left">{{ costumerData.name }}</h1>
-			<h2 class="shrink-0 max-w-fit text-right tabular-nums text-lg">{{ formatMoney(costumerData.totalDebt) }}</h2>
+			<h1 class="text-xl xs:text-2xl font-medium text-left">{{ costumerData.name }}</h1>
+			<h2 class="shrink-0 max-w-fit text-right tabular-nums text-base xs:text-lg">{{ formatMoney(costumerData.totalDebt) }}</h2>
 		</div>
 
 		<div class="space-y-6 text-left xs:mx-10 mx-4 grid-cols-">
 			<div class="flex place-content-between space-x-8 border-l-2 border-emerald-400" v-for="debt in costumerDebts" :key="debt.id">
 				<div class="grow ml-4">
 					<div>
-						<p class="text-sm font-light">{{ getDateString((debt.dateTime.toJSON() as any)["seconds"] * 1000) }}</p>
-						<p class="text-xl font-medium">{{ capitalize(debt.description) }}</p>
+						<p class="text-xs xs:text-sm font-light">{{ getDateString((debt.dateTime.toJSON() as any)["seconds"] * 1000) }}</p>
+						<p class="text-lg xs:text-xl font-medium">{{ capitalize(debt.description) }}</p>
 					</div>
-					<p class="align-bottom tabular-nums underline decoration-solid decoration-emerald-500">{{ formatMoney(debt.price) }}</p>
+					<p class="text-sm xs:text-base align-bottom tabular-nums underline decoration-solid decoration-emerald-500">{{ formatMoney(debt.price) }}</p>
 				</div>
 				<div class="space-x-3 shrink-0 my-auto min-w-fit">
 					<EditDebtModal :debt="debt" :costumerName="costumerData.name"/>
